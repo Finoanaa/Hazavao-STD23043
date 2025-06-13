1a84e4684f8e8979622003d8ff131568c67478ba
Finoana Idealy <154631122+Finoanaa@users.noreply.github.com>
2025-06-13 09:09:27 +0300

    Create HazavaoController.java

---
+++ src/main/java/com/hei/fy/endpoint/rest/controller/HazavaoController.java
@@ -0,0 +1,21 @@
+package com.hei.fy.endpoint.rest.controller;
+
+import com.hei.fy.service.ChatGptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HazavaoController {
+
+    private final ChatGptService chatGptService;
+
+    public HazavaoController(ChatGptService chatGptService) {
+        this.chatGptService = chatGptService;
+    }
+
+    @GetMapping("/hazavao")
+    public String hazavao(@RequestParam String teny) {
+        return chatGptService.getMalagasyDefinition(teny);
+    }
+}
